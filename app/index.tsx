60d0c5f8af0d5591d89ba14aa150ff6d720ad405
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-12-03 11:25:11 +0100

    added. StickyHeader interface

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
-
+import StickyHeader from "@/components/my/StickyHeader";
 export default function Settings() {
-  return <Text>Sono Settings</Text>;
+  return (
+    <StickyHeader
+      name="Devin Shelton"
+      email="lehiedus@gmail.com"
+      modeAccount="Standard Account"
+    />
+  );
 }
