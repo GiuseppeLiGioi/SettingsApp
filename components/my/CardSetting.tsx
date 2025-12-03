73f92782d8c7b6cd6df8dcf693affbd1e4b7d439
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-12-03 13:36:00 +0100

    fixed. responsive width

--- components/my/CardSetting.tsx
+++ components/my/CardSetting.tsx
@@ -2,6 +2,10 @@ import Files from "@/assets/icons/settingsTsx/Files";
 import Mode from "@/assets/icons/settingsTsx/Mode";
 import Payment from "@/assets/icons/settingsTsx/Payment";
 import Settings from "@/assets/icons/settingsTsx/Settings";
+
+import Document from "@/assets/icons/settingsTsx/Document";
+import HelpIcon from "@/assets/icons/settingsTsx/HelpIcon";
+import VitaiLogo from "@/assets/icons/settingsTsx/VitaiLogo";
 import { ScrollView, StyleSheet, View } from "react-native";
 import SingleRowCard from "./SingleRowCard";
 export default function CardSetting() {
@@ -12,7 +16,21 @@ export default function CardSetting() {
     { svg: Mode, name: "Dark Mode", marginLeft: 10, switchProp: true },
   ];
 
-  const rows2 = [];
+  const rows2 = [
+    { svg: VitaiLogo, name: "About Aivi", marginLeft: 0, switchProp: false },
+    {
+      svg: HelpIcon,
+      name: "Help & Support",
+      marginLeft: 10,
+      switchProp: false,
+    },
+    {
+      svg: Document,
+      name: "Privacy and Policy",
+      marginLeft: 10,
+      switchProp: false,
+    },
+  ];
 
   return (
     <ScrollView>
@@ -29,7 +47,7 @@ export default function CardSetting() {
       </View>
 
       <View style={styles.containerCard}>
-        {rows.map((r, index) => (
+        {rows2.map((r, index) => (
           <SingleRowCard
             key={index}
             svg={r.svg({})}
@@ -46,5 +64,8 @@ export default function CardSetting() {
 const styles = StyleSheet.create({
   containerCard: {
     margin: 20,
+    width: "90%",
+    alignSelf: "center",
+    maxWidth: 900,
   },
 });
